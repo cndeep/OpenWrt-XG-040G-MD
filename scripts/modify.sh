68303d0ba980501a71305187bea34ece95df427f
cndeep <145043517+cndeep@users.noreply.github.com>
2026-04-16 20:44:39 +0800

    Add files via upload

---
+++ scripts/modify.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Modify version and ip
+# 此脚本在 openwrt/ 目录下运行，在 feeds install 之后执行
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+
+# Modify version
+sed -i 's/OpenWrt/DeepWrt/g' package/base-files/image-config.in
+sed -i 's/OpenWrt/DeepWrt/g' include/version.mk
+sed -i 's/OpenWrt/DeepWrt/g' package/base-files/files/bin/config_generate
