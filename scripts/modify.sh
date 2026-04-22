1d2d79c527bfe0100c1492c8a962996105dc5ddf
cndeep <145043517+cndeep@users.noreply.github.com>
2026-04-22 12:42:17 +0800

    Update modify.sh

--- scripts/modify.sh
+++ scripts/modify.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Modify version and ip
 # 此脚本在 openwrt/ 目录下运行，在 feeds install 之后执行
+curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
